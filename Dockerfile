c0d1b477276e1a65d0e2671226fc124e69c80a08
eze <ezequielgaray37@gmail.com>
2023-07-03 15:08:26 -0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node
+
+
+WORKDIR /app
+
+
+COPY package*.json ./
+
+
+RUN npm install
+
+
+COPY . .
+
+
+EXPOSE 8080
+
+
+CMD [ "npm", "start" ]
--- app.js
+++ app.js
@@ -155,17 +155,17 @@ import smsRouter from  './src/routes/sms.Router.js'
 
 
 
-console.log(`Es cluster primario? : ${cluster.isPrimary}`);
+//console.log(`Es cluster primario? : ${cluster.isPrimary}`);
 if (cluster.isPrimary) {
     const numeroProcesadores = cpus().length;
-    console.log(`NumeroProcesadores en esta maquina: ${numeroProcesadores}`);
-    console.log("Proceso primario, generando Fork para un trabajador.");
+    //console.log(`NumeroProcesadores en esta maquina: ${numeroProcesadores}`);
+    //console.log("Proceso primario, generando Fork para un trabajador.");
     for (let i = 0; i < numeroProcesadores - 1; i++) {
         cluster.fork();
     }
 } else {
-    console.log("Este es un proceso Fork! Soy un worker!!");
-    console.log(`Soy un proceso worker con el id: ${process.pid}`);
+    //console.log("Este es un proceso Fork! Soy un worker!!");
+    //console.log(`Soy un proceso worker con el id: ${process.pid}`);
 
     const app = express();
     const PORT = config.port;
